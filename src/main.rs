68fae348e3b9300c847c3243c55306584d52e3af
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-02-04 04:10:33 +0100

    reporteer: try different approach #2

--- src/main.rs
+++ src/main.rs
@@ -114,7 +114,7 @@ async fn main() -> std::io::Result<()> {
     info!("Starting server on port {}", config.server_port());
     let enclave_key: Vec<u8> = match EnclaveKeys::get_derived_key() {
         Ok(derived_key) => {
-            let key_vec = derived_key.to_vec();
+            let key_vec = derived_key.as_ref().to_vec();
             if key_vec.len() < 32 {
                 warn!("Derived key too short: {} bytes", key_vec.len());
                 return Ok(());
